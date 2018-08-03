8ba2a66103e8583dd19d841260f17e63a645afed
Juan Tenezaca <tenj34@gmail.com>
2018-08-03 19:04:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist, :genre
+
   @@count = 0
   @@artists = []
   @@genres = []
@@ -8,6 +9,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end
 
 end
