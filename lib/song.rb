fb9aa47705aaf7f263fd2076db31cca73649729f
Juan Tenezaca <tenj34@gmail.com>
2018-08-03 19:03:39 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+end
